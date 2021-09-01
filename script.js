8098a235a87f273ad7ed8472b3e62987decda2bb
nadjaporon <poronnadja@gmail.com>
2021-09-01 11:46:38 +0200

    style.scss changed position attribute to: fixed

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
+// DIVs
 const addBook = document.querySelector('.addBook');
 const bookForm = document.querySelector('.bookForm');
 const bookFormHeader = document.querySelector('.bookForm >h2');
 const cancel = document.querySelector('#cancel');
 const submit = document.querySelector('#submit');
 
-// Form
+// Form Data Inputs
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const read = document.querySelector('#readBook')
 const error = document.querySelector('#error');
 
-// Array Buch
+// Array Book
 let books = [];
 
-/*
-let book1 = new Book('Momo', 'Michael Ende', 304, 'false');
-let book2 = new Book('Der Name der Rose', 'Umberto Eco', 655, 'true');
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
 
-console.table(book1);
-console.table(book2);
-*/
 showBooks();
 
 addBook.addEventListener('click', () => {
@@ -43,6 +44,10 @@ submit.addEventListener('click', () => {
     }
 });
 
+//////////////////////////////////////////////////////
+//      Click Events => edit, delete, read book     //
+//////////////////////////////////////////////////////
+
 window.addEventListener('click', (e) => {
     let element = e.target;
 
@@ -52,29 +57,44 @@ window.addEventListener('click', (e) => {
         return id;
     }
 
-    // Buch löschen
-    if (element.classList.contains('fa-trash')) {
 
+    function storeLocal(array) {
+        localStorage.setItem('bookshelfBook', JSON.stringify(array));
+    }
+
+    // Buch löschen
+    if (element.classList.contains('delete-book')) {
         id = getId();
         books.splice(id, 1);
-        localStorage.setItem('bookshelfBook', JSON.stringify(books));
+
+        storeLocal(books);
+
+        /*
+        removeElement = element.parentElement.parentElement;
+        // removeElement.remove();
+
+        let testId = Array.from(removeElement.parentNode.children).indexOf(removeElement);
+
+        console.log(testId);
+        */
+
         createHTML();
     }
 
     // Status gelesen ändern
-    if (element.classList.contains('fa-book-open')) {
+    if (element.classList.contains('read-book')) {
         id = getId();
         if (books[id].read == 'true') {
             books[id].read = 'false';
         } else {
             books[id].read = 'true';
         }
-        localStorage.setItem('bookshelfBook', JSON.stringify(books));
+        storeLocal(books);
         createHTML();
     }
 
     // Buch bearbeiten
-    if (element.classList.contains('fa-edit')) {
+    if (element.classList.contains('edit-book')) {
         id = getId();
         bookForm.style.display = 'block';
 
@@ -84,6 +104,7 @@ window.addEventListener('click', (e) => {
         title.value = books[id].title;
         author.value = books[id].author;
         pages.value = books[id].pages;
+
         if (books[id].read == 'true') {
             read.checked = true;
         } else {
@@ -91,18 +112,6 @@ window.addEventListener('click', (e) => {
         }
     }
 });
-
-
-
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-
-
 
 
 function addBookToLibrary(type) {
@@ -121,7 +130,7 @@ function addBookToLibrary(type) {
                 newBook.read = 'false';
             }
             books.push(newBook);
-     
+
             bookForm.style.display = 'none';
 
             localStorage.setItem('bookshelfBook', JSON.stringify(books));
@@ -159,24 +168,22 @@ function showBooks() {
 }
 
 function createHTML() {
+    // DIV where HTML elements will get into
     const booksContainer = document.querySelector('.books');
 
-    booksContainer.innerHTML = '';
-
     let retrievedData = localStorage.getItem('bookshelfBook');
-
     books = JSON.parse(retrievedData);
 
+    booksContainer.innerHTML = '';
 
     // create HTML Elements
     for (let i = 0; i < books.length; i++) {
 
         // <div> with Classname 'bookItem'
         let bookItem = document.createElement('div');
-
         bookItem.classList.add('bookItem');
 
-        // <h2>
+        // <h2> Title
         let h2Tag = document.createElement('h2');
         h2Tag.textContent = books[i].title;
         bookItem.appendChild(h2Tag);
@@ -202,31 +209,29 @@ function createHTML() {
             pTagRead.classList.add('green');
         }
 
-        // <p> Icons
-        let pTagIcons = document.createElement('p');
+        /////////////////////////////////////////////////////////////
+        //      Icons: Edit * Delete *  Change Status Read         //
+        /////////////////////////////////////////////////////////////
 
-        // Add class
+        // <p> Icons + Add class
+        let pTagIcons = document.createElement('p');
         pTagIcons.classList.add('icons');
 
-        
-
-        // <i> Edit Icon
+        // <i> Edit Icon + Add class + id
         iTagEdit = document.createElement('i');
-        iTagEdit.classList.add('fas', 'fa-edit');
+        iTagEdit.classList.add('fas', 'fa-edit', 'edit-book');
         iTagEdit.id = 'edit-' + i;
         pTagIcons.appendChild(iTagEdit);
 
-        // <i> Trash Icon
+        // <i> Trash Icon + Add class + id
         iTagTrash = document.createElement('i');
-        iTagTrash.classList.add('fas', 'fa-trash');
+        iTagTrash.classList.add('fas', 'fa-trash', 'delete-book');
         iTagTrash.id = 'trash-' + i;
         pTagIcons.appendChild(iTagTrash);
 
-        // deleteBook(i);
-
-        // <i> Read Icon
+        // <i> Read Icon + Add class + id
         iTagRead = document.createElement('i');
-        iTagRead.classList.add('fas', 'fa-book-open');
+        iTagRead.classList.add('fas', 'fa-book-open', 'read-book');
         iTagRead.id = 'read-' + i;
         pTagIcons.appendChild(iTagRead);
 
